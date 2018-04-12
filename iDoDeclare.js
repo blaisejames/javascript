91c99ef60c889996044dd1393e4531874c79ca1e
Blaise James <blaise@blaisejames.com>
2018-04-12 13:19:01 -0400

    committing IDoDeclare.js

---
+++ iDoDeclare.js
@@ -0,0 +1,33 @@
+// 4 numbers
+var pi = 3.14159;
+console.log("The value of pi is", pi);
+
+var time = 17633;
+console.log("The number of days since Jan 1, 1970 is", time);
+
+var float = 6.2;
+console.log("A decimal number:", float);
+
+var neg = -42;
+console.log("A negative number:", neg);
+
+// 3 strings
+var bruce = "Springsteen";
+console.log("Last name of a famous Bruce:", bruce);
+
+var hand = "left";
+console.log("You are " + hand + " handed");
+
+var stooge = "Curley";
+console.log("Your favorite of the 3 Stooges:", stooge)
+
+// 2 Booleans
+var bool = true;
+console.log("She loves me:", bool);
+
+var f = false;
+console.log("She loves me not:", f);
+
+// undefined
+var arr = []
+console.log("Produces an undefined array:", arr);
