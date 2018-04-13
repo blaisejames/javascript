a1d80cfe91c8ea8b646d0cd46df0adeb89ea1749
Blaise James <blaise@blaisejames.com>
2018-04-13 18:48:23 -0400

    second update of forAFewBillion.js

--- forAFewBillion.js
+++ forAFewBillion.js
@@ -2,6 +2,5 @@
 var dayValue = 0.1;
 for (var i = 2; i <= 30; i++) {
     dayValue = dayValue + dayValue;
-    console.log(dayValue);
 }
 console.log("After 30 days, the servant would have a reward of $" + dayValue);
