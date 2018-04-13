e1f65ece58c3aed1606cdb53c921cd46acab268f
Blaise James <blaise@blaisejames.com>
2018-04-13 17:52:18 -0400

    Adding ifYouDontMind.js

---
+++ WhileYouWait.js
@@ -0,0 +1,18 @@
+// While You Wait
+for (var daysUntilMyBirthday = 60; daysUntilMyBirthday >= 0; daysUntilMyBirthday--) {
+    if (daysUntilMyBirthday >= 30) {
+    console.log(daysUntilMyBirthday + " days until my birthday. Such a long time... :(");
+    }
+    if (daysUntilMyBirthday < 30 && daysUntilMyBirthday >= 5) {
+        console.log(daysUntilMyBirthday + " days until my birthday. I'm getting excited!");
+    }
+    if (daysUntilMyBirthday < 5 && daysUntilMyBirthday > 1) {
+        console.log(daysUntilMyBirthday + " DAYS UNTIL MY BIRTHDAY!!");
+    }
+    if (daysUntilMyBirthday == 1) {
+        console.log(daysUntilMyBirthday + " DAYS UNTIL MY BIRTHDAY!!!");
+    }
+    if (daysUntilMyBirthday == 0) {
+        console.log("\nIT'S MY BIRTHDAY...GOTTA LOVE ME!!!!\n\n♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*\n♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪\n*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«");
+    }
+}
