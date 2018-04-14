e044967046fcd7d52055c47f9af73b2a849e0233
Blaise James <blaise@blaisejames.com>
2018-04-14 13:59:42 -0400

    first commit of studentsInstructors.js

--- numbersOnly.js
+++ numbersOnly.js
@@ -1,11 +1,13 @@
 // Numbers Only
-function numbersOnly(arr){
-    var newArray = [];
-    for (var i = 0; i < arr.length; i++) {
-        if (typeof arr[i] == "number") {
-            newArray.push(arr[i]);
-        }
-    }
-    return newArray;
-}
-console.log(numbersOnly([1, "apple", -3, "orange", 0.5]));
+// function numbersOnly(arr){
+//     var newArray = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         if (typeof arr[i] == "number") {
+//             newArray.push(arr[i]);
+//         }
+//     }
+//     return newArray;
+// }
+// console.log(numbersOnly([1, "apple", -3, "orange", 0.5]));
+
+console.log(1 && "");
