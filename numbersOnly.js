7da8c833e6e31fdf544e9eadbddae01eeebc8a6b
Blaise James <blaise@blaisejames.com>
2018-04-14 11:48:17 -0400

    first commit of numbersOnly.js

---
+++ numbersOnly.js
@@ -0,0 +1,11 @@
+// Numbers Only
+function numbersOnly(arr){
+    var newArray = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (typeof arr[i] == "number") {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+}
+console.log(numbersOnly([1, "apple", -3, "orange", 0.5]));
